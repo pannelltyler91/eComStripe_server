1177d23028e62bc6452bdff9204b729144826dfe
unknown <you@example.com>
2022-01-17 19:59:42 -0500

    basic working project

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-require('dotenv').config()
+require("dotenv").config();
 //hello
-const express = require('express')
+const express = require("express");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 const app = express();
-const cors = require('cors')
-const port = process.env.PORT || 3001
-const db = require('./models')
-const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+const cors = require("cors");
+const port = process.env.PORT || 3001;
+const db = require("./models");
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -14,31 +16,37 @@ app.use(
     extended: true,
   })
 );
-app.get('/menu', (req,res) => {
- db.menuItems.findAll({})
- .then((items) =>{
-   console.log(items)
-   res.json({menuItems:items})
- })
-})
 
-app.post('/create_checkout_session', async (req,res) =>{
-  try{
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types:['card'],
-      mode:'payment',
-      line_items:'',
-      success_url:`${process.env.SERVER_URL}/success.html`,
-      cancel_url:`${process.env.SERVER_URL}/cancel.html`
-    })
-    res.json({url:session.url})
-  }catch(e){
-    res.status(500).json({error:e.message})
-  }
-  console.log(req.body)
-})
+//add async back in before req, res
+app.post("/create_checkout_session", async (req, res) => {
+try{
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types:['card'],
+    mode:'payment',
+    line_items:req.body.items.map((item) => {
 
+      return{
+        price_data:{
+          currency:'usd',
+          product_data:{
+            name:item.name
+          },
+          unit_amount:item.price * 100
+        },
+        quantity:item.quantity
+      }
+    }),
+    success_url:`${process.env.SERVER_URL}/success`,
+    cancel_url:`${process.env.SERVER_URL}/cancel`
+  })
+res.json({ url: session.url });
+  
+
+}catch(e){
+  res.status(500).json({error:e.message})
+}
+});
 
 app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
-  });
+  console.log(`Listening on port: ${port}`);
+});
